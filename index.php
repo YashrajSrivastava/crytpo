be2edf331aa0ef11e12d546a2e76b60d228b56f1
YashrajSrivastava <129870062+YashrajSrivastava@users.noreply.github.com>
2023-04-20 16:01:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <title>Crypto</title>
+</head>
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block   w-100" style="height: 35rem;" src="https://cdn.pixabay.com/photo/2018/01/12/04/23/currency-3077534__340.jpg" alt="First slide">
+    </div>
+ 
+</div>
+    
+
+    
+
+
+    
+    
+    <section class="my-5">
+        <div class="py-5">
+          <h2 class="text-center">
+            Know About Crypto
+          </h2>
+        </div>
+        <div class="container-fluid">
+          <div class="row ">
+            <div class="col-lg-6 col-md-6 col-12">
+    
+              <img src="https://cdn.pixabay.com/photo/2021/09/06/12/40/cryptocurrency-6601591_960_720.jpg " class="img-fluid aboutimage" alt="">
+            </div>
+    
+            <div class="col-lg-6 col-md-6 col-12">
+              <h1 class="display-4">Crypto Life</h1>
+              <p class="py-3">“If you don’t believe it or don’t get it, I don’t have the time to try to convince you, sorry. Cryptocurrency is decentralized digital money that is based on blockchain technology and secured by cryptography. To understand cryptocurrency, one needs to first understand three terminologies – blockchain, decentralization, and cryptography.” – Satoshi Nakamoto.</p>
+              <a href="https://html.crumina.net/woox-crypto/index.html" class="btn btn-success">Know More </a>
+            </div>
+    
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+      <footer >
+        
+        
+          <p class="p-3 bg-dark text-white text-center">@CryptoLife <a href="#" class="fa fa-facebook m-2"></a>
+    <a href="#" class="fa fa-twitter m-2"></a> </p>
+            
+
+         
+
+          
+      </footer>
+
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
